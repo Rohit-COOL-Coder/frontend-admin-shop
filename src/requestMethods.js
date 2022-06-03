a8793339f82873d5a713e11944a59e8331b7885a
rohit kumar sah <rohit.sah892@atmecs.com>
2022-06-03 08:20:12 +0530

    token updated

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -3,7 +3,7 @@ import { useSelector } from "react-redux"
 
 const baseUrl="https://rohitshop.herokuapp.com/api"
 const data=localStorage.getItem("persist:root")
-const token="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyNjQxNTZjNTY5ZWI1MmQ0OGI1NDIyYiIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY1MzYwOTA5MCwiZXhwIjoxNjUzODY4MjkwfQ.68zJnqnAJqXZ51B9j0fym1GGpJHtaFtWQtcwJq37E_c"
+const token="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyNjQxNTZjNTY5ZWI1MmQ0OGI1NDIyYiIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY1NDIyNDM3NywiZXhwIjoxNjU0NDgzNTc3fQ.VPNaN32L0wAS-Go4-x0yCxYnS8wSkwx12R9WB6_G3vQ"
 // JSON.parse(JSON.parse(data)?.user).currentUser?.accessToken
 export const publicRequest=axios.create({
     baseURL:baseUrl
